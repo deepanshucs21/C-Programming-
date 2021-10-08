533722ac8bae04065da50837e88161eed3c27d3c
deepanshucs21 <92175280+deepanshucs21@users.noreply.github.com>
2021-10-08 09:30:31 -0700

    Add files via upload
    C Program To Print An Integer

---
+++ integer.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{   
+    int number;   
+    printf("Enter an integer: ");
+    scanf("%d", &number);//for input 
+    printf("You entered: %d", number);//for output
+    return 0;
+}
