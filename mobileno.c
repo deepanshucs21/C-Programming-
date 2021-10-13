6a449fb6f8f018cf6870eed8f2c81e2c28f16fe2
Deepanshu Sharma <92175280+deepanshucs21@users.noreply.github.com>
2021-10-13 22:50:15 +0530

    C Program To Input Mobile Number And Print It

---
+++ mobileno.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    long long int mobile;
+    printf("Enter Mobile Number:");
+    scanf("%lli",&mobile);
+    printf("Mobile Number Is :%lli",mobile);
+    return 0;
+}
