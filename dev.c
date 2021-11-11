81f59bfdcc16af0dfe7ad03692b71a557c406593
Deepanshu Sharma <92175280+deepanshucs21@users.noreply.github.com>
2021-11-11 10:47:46 +0530

    Add files via upload
    Program To Check Whether A Number Is Positive , Negative Or Zero Using Switch Statements

---
+++ dev.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+char character;
+printf("Enter A Character");
+scanf("%c",&character);
+switch(character)
+{
+case 'a':printf("vowel");
+         break;
+case 'e':printf("vowel");
+         break;
+case 'i':printf("vowel");
+         break;
+case 'o':printf("vowel");
+         break;
+case 'u':printf("vowel");
+         break;
+case 'A':printf("vowel");
+         break;
+case 'E':printf("vowel");
+         break;
+case 'I':printf("vowel");
+         break;
+case 'O':printf("vowel");
+         break;
+case 'U':printf("vowel");
+         break;
+default:printf("Consonant");
+}
+return 0;
+}
+
+
